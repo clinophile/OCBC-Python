ddd2d28f42cc5cba0d5878419399e0909bfaf304
Dini Puspita <clinophile13@gmail.com>
2021-12-24 11:22:57 +0700

    add assignment3 for deploy

---
+++ Assignment03/README.md
@@ -0,0 +1,3 @@
+open with http://h8ocbc-deployment-014.herokuapp.com
+
+for test swagger http://h8ocbc-deployment-014.herokuapp.com/api/ui
--- Assignment03/config.py
+++ Assignment03/config.py
@@ -13,7 +13,7 @@ app = connex_app.app
 
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'people.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iycayxycmbjhno:b1a388ff175a11f5e92dfa4a98341d9ad0e33e97bc3765e3caf158de6ca220f7@ec2-3-214-4-239.compute-1.amazonaws.com:5432/d4enivuk02sdfv'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
