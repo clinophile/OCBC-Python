5a8399a95f8324b3b1baff2bb566d21c74342fc2
Dini Puspita <clinophile13@gmail.com>
2021-12-24 11:45:43 +0700

    modif assignment3

--- Assignment03/config.py
+++ Assignment03/config.py
@@ -13,7 +13,7 @@ app = connex_app.app
 
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iycayxycmbjhno:b1a388ff175a11f5e92dfa4a98341d9ad0e33e97bc3765e3caf158de6ca220f7@ec2-3-214-4-239.compute-1.amazonaws.com:5432/d4enivuk02sdfv'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xfnihrjhgrdofy:532155c6ed7d56c9a8460b9794c18a0bc633ee3ce4cd1151c952ef5c0c48a58c@ec2-3-214-4-239.compute-1.amazonaws.com:5432/d966ql180adoh5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
